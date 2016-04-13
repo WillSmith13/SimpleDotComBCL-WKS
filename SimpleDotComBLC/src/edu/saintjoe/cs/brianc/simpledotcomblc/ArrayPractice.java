34b9dd2061c4b8555e24294b3c77ec12bcdce526
wills <wills@localhost>
2016-04-13 14:15:45 -0400

    New code input

---
+++ SimpleDotComBLC/src/edu/saintjoe/cs/brianc/simpledotcomblc/ArrayPractice.java
@@ -0,0 +1,40 @@
+package edu.saintjoe.cs.brianc.simpledotcomblc;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// Create a new ref variable which ref an array of Strings;
+		String[] names;
+		
+		// Create a new array that will hold ref to 5 Strings
+		names = new String[5];
+		
+		names[0] = "Arnold";
+		names[1] = "Luis";
+		names[1] = "Elmo";
+		names[2] = "Luke";
+		names[3] = "Lee";
+		names[4] = "Elmo";
+		
+		// Print all the names in the array 
+		for ( String oneName: names) {
+			System.out.println(" We just got " + oneName);
+			
+		}
+		
+		String result = "Not found ";
+		// Just print Elmo's name 
+		for (String oneName: names ) {
+			if (oneName == "Elmo") {
+				result = "Found!!!";
+			}
+		}
+		System.out.println("Value of search result: " + result);
+
+	}
+
+
+
+	}
+
+
